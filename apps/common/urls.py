f0d5fde4ba50084c1bc5f3e04cd49207e598b447
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2024-11-15 20:25:25 +0500

    New

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -6,7 +6,6 @@ from apps.common.views import health_check_redis
 app_name = "common"
 
 urlpatterns = [
-    path("VersionHistory/", VersionHistoryView.as_view(), name="version-history"),
 
     path("health-check/redis/", health_check_redis, name="health-check-redis"),
 ]
