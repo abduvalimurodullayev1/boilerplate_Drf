fc992aeeed22d1c8de865bae7279148f7aa45bb0
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2024-11-17 20:38:23 +0500

    add jwt_auth

--- core/settings/base.py
+++ core/settings/base.py
@@ -48,6 +48,8 @@ THIRD_PARTY_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
     ),
     "DEFAULT_FILTER_BACKENDS": (
         "django_filters.rest_framework.DjangoFilterBackend",
