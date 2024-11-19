4f3070041c37962cf9e24bbfbdf2be221bef0484
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2024-11-19 18:56:12 +0500

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -49,6 +49,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication', 
+
 
     ),
     "DEFAULT_FILTER_BACKENDS": (
