22ff69bbc8a54bcc71c50e3212946c1dd82e6aa5
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2024-12-06 14:04:02 +0500

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -230,7 +230,6 @@ SWAGGER_SETTINGS = {
 }
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
