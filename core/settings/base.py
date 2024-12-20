b3372ba9b568de8106a3c2a5f39215724ab43576
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2024-12-20 09:55:29 +0500

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import environ
 
-# Build paths inside the project like this: BASE_DIR / 
+# Build paths inside the project like this: 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # READING ENV
